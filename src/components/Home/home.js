da4b939b48a47c95a56840e15bd90b3ed42713bc
Shyket <munimulislam@iut-dhaka.edu>
2021-06-11 12:01:18 +0600

    added css to homepage

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,15 +1,18 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-      <div>
-        <p>HomePage</p>
+  return (
+    <div>
+      <p>HomePage</p>
+      <p>
         <Link to="/Register">Register</Link>
-        <Link to="/Login">Login</Link> 
-      </div>
-    );
-}
-
+      </p>
+      <p>
+        <Link to="/Login">Login</Link>
+      </p>
+    </div>
+  );
+};
 
 export default Home;
