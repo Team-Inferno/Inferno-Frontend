cc80958e5ddc84652ee7282f0a38576c4741daa2
Shyket <munimulislam@iut-dhaka.edu>
2021-06-11 11:57:28 +0600

    adeed links to homepage

--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { Link } from "react-router-dom";
 
 const Home = () => {
     return (
+      <div>
         <p>HomePage</p>
-    )
+        <Link to="/Register">Register</Link>
+        <Link to="/Login">Login</Link> 
+      </div>
+    );
 }
 
 
