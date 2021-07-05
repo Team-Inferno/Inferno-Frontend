0f76bf5a380e052d028d0faabac575e27a54b1eb
Aseer Intiser <aseerniloy@gmail.com>
2021-07-06 03:43:29 +0600

    set routes for streams

--- src/routes/index.route.js
+++ src/routes/index.route.js
@@ -4,6 +4,11 @@ import PrivateRoute from './private.route';
 import Login from "../components/Auth/Login/login";
 import Register from "../components/Auth/Register/register";
 import Home from "../components/Home/home";
+import StreamCreate from '../components/streams/StreamCreate';
+import StreamEdit from '../components/streams/StreamEdit';
+import StreamDelete from '../components/streams/StreamDelete';
+import StreamList from '../components/streams/StreamList';
+import StreamShow from '../components/streams/StreamShow';
 
 const Routes = (props) => {
   return (
@@ -12,6 +17,12 @@ const Routes = (props) => {
         <Route exact path="/login" component={Login} />
         <Route exact path="/register" component={Register} />
         <PrivateRoute exact path="/" component={Home} />
+        
+        <Route path="/streams" exact component={StreamList} />
+        <Route path="/streams/delete" exact component={StreamDelete} />
+        <Route path="/streams/show" exact component={StreamShow} />
+        <Route path="/streams/edit" exact component={StreamEdit} />
+        <Route path="/streams/create" exact component={StreamCreate} />
       </Switch>
     </Router>
   );
