00cd344530b1da51763c265b35ac889bcbd75ca7
Shyket <munimulislam@iut-dhaka.edu>
2021-07-09 19:47:30 +0600

    user profile ui completed

--- src/components/Profile/Invite.js
+++ src/components/Profile/Invite.js
@@ -1,9 +1,19 @@
 import React from 'react';
 
 const Invite = (props) => {
-  return(
-    <div>Invite</div>
-   )
+  return (
+    <div className="invite-box">
+      <div className="invite-description">
+        <p>
+          User Mr.Angular Invites you To The server <span>{"Pussy Cat"}</span>
+        </p>
+      </div>
+      <div className="button-area">
+        <button className="invite-accept-button">Accept</button>
+        <button className="invite-decline-button">Decline</button>
+      </div>
+    </div>
+  );
 
  }
 
